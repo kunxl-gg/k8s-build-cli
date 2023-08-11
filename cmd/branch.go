81a77c13f9e7a242e208296299f5a1882b5f694f
kunxl-gg <tiwari.25@iitj.ac.in>
2023-08-11 17:39:34 +0530

    added folder for branching project
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- cmd/branch.go
+++ cmd/branch.go
@@ -1,16 +1,18 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-// createCmd represents the create command
-var createCmd = &cobra.Command{
-	Use:   "create",
+// branchCmd represents the branch command
+var branchCmd = &cobra.Command{
+	Use:   "branch",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -19,26 +21,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		base_url := "https://api.opensuse.org/source"
-		project_name := "home:kunxl.gg"
-		repository_name := "openSUSE_Tumbleweed"
-		architecture_name := "x86_64"
-		package_name := "k8s"
-
-		url := base_url + "/" + project_name + "/" + repository_name + "/" + architecture_name + "/" + package_name + "/_buildinfo"
+		fmt.Println("branch called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(createCmd)
+	rootCmd.AddCommand(branchCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// branchCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// branchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
