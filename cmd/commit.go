ad201a25181f9f97725717ea69e0be5d873af3b2
kunxl-gg <tiwari.25@iitj.ac.in>
2023-08-15 17:14:55 +0530

    done with commit end point as well
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- cmd/commit.go
+++ cmd/commit.go
@@ -4,6 +4,11 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/kunxl-gg/lfx-lezgooo/config"
+	"github.com/kunxl-gg/lfx-lezgooo/helpers"
 	"github.com/spf13/cobra"
 )
 
@@ -19,12 +24,25 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-
+		// initailizing variables
+		base_url := config.BaseUrl
+		project_name := "home:" + config.Username
+		package_name := "hello-world"
 		
+		// the final url
+		url := base_url + "source/" + project_name + "/" + package_name + "?cmd=commit"
+
+		resp := helpers.APICall("POST", url)
+
+		// printing the response
+		body, err := ioutil.ReadAll(resp.Body)
+		helpers.CheckError(err)
+
+		fmt.Println(string(body))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(commitCmd)
-	
+
 }
